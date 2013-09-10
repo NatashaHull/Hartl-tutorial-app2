643d213cddbadfb90fcda2127f0e8f0ab50415ac
Natasha Hull <musicalnat@gmail.com>
2013-09-09 20:49:32 -0700

    Finish user edit, update, index and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,19 +1,44 @@
 include ApplicationHelper
 
-def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
+def signup(options={})
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirm Password", with: "foobar"
+  options.each do |k, v|
+    fill_in k, with: v
+  end
 end
 
-def signup(options={})
-	fill_in "Name",         with: "Example User"
-    fill_in "Email",        with: "user@example.com"
-    fill_in "Password",     with: "foobar"
-    fill_in "Confirmation", with: "foobar"
-    options.each do |k, v|
-    	fill_in k, with: v
-    end
+def signin(user, options={})
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  options.each do |k, v|
+    fill_in k, with: v
+  end
+end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    signin(user)
+    click_button "Sign in"
+  end
+end
+
+def update(user, options={})
+  fill_in "Name",             with: user.name
+  fill_in "Email",            with: user.email
+  fill_in "Password",         with: user.password
+  fill_in "Confirm Password", with: user.password
+  options.each do |k, v|
+    fill_in k, with: v
+  end
 end
 
 RSpec::Matchers.define :have_error_message do |message|
