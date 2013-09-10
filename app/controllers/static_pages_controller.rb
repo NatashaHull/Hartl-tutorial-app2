e22d1c8cc4f3bdde85668a5b83044adf2f601773
Natasha Hull <musicalnat@gmail.com>
2013-09-10 14:16:45 -0700

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+  	if signed_in?
+  		@user = current_user
+	  	@micropost = @user.microposts.build
+	  	@microposts = @user.microposts.paginate(page: params[:page])
+	  	@feed_items = @user.feed.paginate(page: params[:page])
+	  end
   end
   
   def help
